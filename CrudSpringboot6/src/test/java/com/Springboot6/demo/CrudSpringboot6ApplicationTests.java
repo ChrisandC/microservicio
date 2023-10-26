8fbd89d3a405ac82db8e6ee85af25b088127d6ec
ChrisandC <148001790+ChrisandC@users.noreply.github.com>
2023-10-26 15:34:45 -0500

    Add files via upload

---
+++ CrudSpringboot6/src/test/java/com/Springboot6/demo/CrudSpringboot6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Springboot6.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringboot6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
